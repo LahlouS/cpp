66ffd7ab631b545db20d4ff8d7383d7f15338bdb
Sacha LAHLOU <slahlou@e1r3p17.clusters.42paris.fr>
2022-09-29 18:10:24 +0200

    seems ok

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -5,14 +5,13 @@
 int const Fixed::_fract = 8;
 
 Fixed::Fixed() : _nb(0){
-	std::cout << "default constructor has been called\n";
+	std::cout << "Default constructor has been called\n";
 	return ;
 }
 
 Fixed::Fixed(Fixed const & cpy){
-
-	std::cout << "Copy constructor called\n";
-	*this = cpy;
+	std::cout << "Copy constructor has been called\n";
+	this->_nb = cpy._nb;
 }
 
 Fixed::~Fixed(){
@@ -25,6 +24,7 @@ Fixed::Fixed(int const to_convert){
 }
 
 Fixed::Fixed(float const to_convert){
+	std::cout << "Constructor with float\n";
 	this->_nb = roundf(to_convert * (1 << this->_fract));
 }
 
@@ -46,9 +46,107 @@ float	Fixed::toFloat(void) const{
 }
 
 Fixed & Fixed::operator=(Fixed const & to_assign){
-	std::cout << "overload of assignement operator has been called\n";
+	std::cout << "Assignement operator overload called\n";
 	this->_nb = to_assign.getRawBits();
 	return (*this);
+}
+
+bool Fixed::operator<(Fixed const & to_assign) const{
+	return (this->_nb < to_assign._nb);
+}
+
+bool Fixed::operator<=(Fixed const & to_assign) const{
+	return (this->_nb <= to_assign._nb);
+}
+
+bool Fixed::operator>(Fixed const & to_assign) const{
+	return (this->_nb > to_assign._nb);
+}
+
+bool Fixed::operator>=(Fixed const & to_assign) const{
+	return (this->_nb >= to_assign._nb);
+}
+bool Fixed::operator==(Fixed const & to_assign) const{
+	return (this->_nb == to_assign._nb);
+}
+
+bool Fixed::operator!=(Fixed const & to_assign) const{
+	return (this->_nb != to_assign._nb);
+}
+
+Fixed	Fixed::operator+(Fixed const & to_add) const{
+	return (Fixed(this->toFloat() + to_add.toFloat()));
+}
+
+Fixed	Fixed::operator-(Fixed const & to_sub) const{
+	return (Fixed(this->toFloat() - to_sub.toFloat()));
+}
+
+Fixed	Fixed::operator/(Fixed const & to_div) const{
+	return (Fixed(this->toFloat() / to_div.toFloat()));
+}
+
+Fixed	Fixed::operator*(Fixed const & to_time) const{
+	return (this->toFloat() * to_time.toFloat());
+}
+
+Fixed & Fixed::operator++(void){
+	this->_nb += 1;
+	return (*this);
+}
+
+Fixed Fixed::operator++(int){
+	Fixed	old(*this);
+	operator++();
+	return(old);
+}
+
+Fixed & Fixed::operator--(void){
+	this->_nb -= 1;
+	return (*this);
+}
+
+Fixed Fixed::operator--(int){
+	Fixed	old(*this);
+	operator--();
+	return(old);
+}
+
+Fixed const & Fixed::min(Fixed const & nb1, Fixed const & nb2){
+	if (nb1 < nb2)
+		return (nb1);
+	else if (nb1 != nb2)
+		return (nb2);
+	std::cout << "both parameters are equal so first parameter is retuned\n";
+	return (nb1);
+}
+
+Fixed const & Fixed::min(Fixed & nb1, Fixed & nb2)
+{
+	if (nb1 < nb2)
+		return (nb1);
+	else if (nb1 != nb2)
+		return (nb2);
+	std::cout << "both parameters are equal so first parameter is retuned\n";
+	return (nb1);
+}
+
+Fixed const & Fixed::max(Fixed const & nb1, Fixed const & nb2){
+	if (nb1 > nb2)
+		return (nb1);
+	else if (nb1 != nb2)
+		return (nb2);
+	std::cout << "both parameters are equal so first parameter is retuned\n";
+	return (nb1);
+}
+
+Fixed const & Fixed::max(Fixed & nb1, Fixed & nb2){
+	if (nb1 > nb2)
+		return (nb1);
+	else if (nb1 != nb2)
+		return (nb2);
+	std::cout << "both parameters are equal so first parameter is retuned\n";
+	return (nb1);
 }
 
 std::ostream & operator<<(std::ostream & o, Fixed const & output){
--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -9,11 +9,30 @@ class Fixed {
 		Fixed(int const to_convert);
 		Fixed(float const to_convert);
 		~Fixed();
+		bool operator<(Fixed const & to_assign) const;
+		bool operator<=(Fixed const & to_assign) const;
+		bool operator>=(Fixed const & to_assign) const;
+		bool operator>(Fixed const & to_assign) const;
+		bool operator==(Fixed const & to_assign) const;
+		bool operator!=(Fixed const & to_assign) const;
+		Fixed operator+(Fixed const & to_assign) const;
+		Fixed operator-(Fixed const & to_assign) const;
+		Fixed operator/(Fixed const & to_assign) const;
+		Fixed operator*(Fixed const & to_assign) const;
+		Fixed & operator++(void);
+		Fixed operator++(int);
+		Fixed & operator--(void);
+		Fixed operator--(int);
+		static Fixed const & min(Fixed const & nb1, Fixed const & nb2);
+		static Fixed const & min(Fixed & nb1, Fixed & nb2);
+		static Fixed const & max(Fixed const & nb1, Fixed const & nb2);
+		static Fixed const & max(Fixed & nb1, Fixed & nb2);
+
 		int		getRawBits( void ) const;
 		void	setRawBits( int const raw );
 		float	toFloat(void) const;
 		int		toInt(void) const;
-		
+
 	private:
 		int _nb;
 		static int const _fract;
