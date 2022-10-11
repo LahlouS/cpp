f9b00aa265215682d5d19300d12f8dabb4a8b04c
Sacha LAHLOU <slahlou@e3r12p10.clusters.42paris.fr>
2022-10-11 16:32:29 +0200

    it seems ok

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -13,25 +13,25 @@ void	programPause(void){
 	std::cin.get(backSlashN);
 }
 
-// int main(){
+int main(){
 
-// 	try{
-// 		Form form("cerfa08", 91, 91);
-// 		std::cout << form;
-// 		Bureaucrat B("sacha", 90);
-// 		Bureaucrat C("luc", 92);
-// 		std::cout << B;
-// 		std::cout << "\n\n\n";
-// 		B.signForm(form);
-// 		std::cout << "\n\n\n";
-// 		C.signForm(form);
-// 		std::cout << form;
-// 	}
-// 	catch(std::exception const & e){
-// 		std::cout << e.what();
-// 	}
-// 	return (0);
-// }
+	try{
+		PresidentialPardonForm form("cerfa08");
+		std::cout << form;
+		Bureaucrat B("sacha", 12);
+		Bureaucrat C("luc", 1);
+		std::cout << B;
+		std::cout << "\n\n\n";
+		B.signForm(form);
+		std::cout << "\n\n\n";
+		C.signForm(form);
+		std::cout << form;
+	}
+	catch(std::exception const & e){
+		std::cout << e.what();
+	}
+	return (0);
+}
 
 // int main(){
 // 	//ShrubberyCreationForm test = ShrubberyCreationForm("Shrubberyfffffff");
@@ -57,12 +57,12 @@ void	programPause(void){
 // 	return (0);
 // }
 
-int main(void){
-{
-	Intern someRandomIntern;
-	Form* rrf;
-	rrf = someRandomIntern.makeForm("Roboto", "Bender");
-	delete rrf;
-}
-return (0);
-}
+// int main(void){
+// {
+// 	Intern someRandomIntern;
+// 	Form* rrf;
+// 	rrf = someRandomIntern.makeForm("Roboto", "Bender");
+// 	delete rrf;
+// }
+// return (0);
+// }
